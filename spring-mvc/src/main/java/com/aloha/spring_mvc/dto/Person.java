f372759b5432445ff6bb61c7f73504a6258a3ed8
okzota4-CHOSUNNY <okzota4@gmail.com>
2026-01-07 19:17:42 +0900

    07_20260107_TIL

---
+++ spring-mvc/src/main/java/com/aloha/spring_mvc/dto/Person.java
@@ -0,0 +1,13 @@
+package com.aloha.spring_mvc.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Person {
+  private String name;
+  private int age;
+
+  private List<String> hobby;
+}
